1e1a79354f2bd701941f633ba8177fb98b0a4ce2
casa <casa@ujo.guru>
2020-03-31 22:17:27 +0300

    function names to more informative

--- terminal-corona.sh
+++ terminal-corona.sh
@@ -3,30 +3,31 @@
 # data source is CSSE at Johns Hopkins University COVID-19 git database
 # casa@ujo.guru 2020
 
-# decorations from deco.sh for standalone
+country_selected="Finland"
+country_list=("Finland" "Sweden" "Estonia" "Russia" "Norway" "Germany" "Spain" "France" "Italy" "Kingdom" "China" "US" )
+source_url="https://github.com/CSSEGISandData/COVID-19/blob/web-data/data/cases_country.csv"
+
+# quick decorations from deco.sh for standalone
 export RED='\033[0;31m'
 export GRN='\033[0;32m'
 export WHT='\033[1;37m'
 export NC='\033[0m'
 export UPDATED="${GRN}UPDATED${NC}\n"
 export FAILED="${RED}FAILED${NC}\n"
+FAILED () {  [ "$1" ] && printf "${WHT}$1:${NC} $FAILED" || printf "$FAILED"  ; }
+UPDATED () { [ "$1" ] && printf "$1: $UPDATED"           || printf "$UPDATED" ; }
 
-FAILED () { [ "$1" ] && printf "${WHT}$1:${NC} $FAILED" || printf "$FAILED"; }
-UPDATED () { [ "$1" ] && printf "$1: $UPDATED" || printf "$UPDATED"; }
 
 corona.main () {
-    location="Finland"
-    report_location="COVID-19/data"     # in the repository (donn't change)
-    country_list=("Finland" "Sweden" "Estonia" "Russia" "Norway" "Germany" "Spain" "France" "Italy" "Kingdom" "China" "US" )
 
     case ${1,,} in
-             status|all) corona.country_current_intrest ;;
-                  short) corona.country_current_oneline "$2";;
-                  table) corona.country_current_table ;;
-           view|display) corona.display "$2" ;;
-                    web) firefox https://github.com/CSSEGISandData/COVID-19/blob/web-data/data/cases_country.csv ;;
-                   help) corona.help ;;
-                      *) corona.country_current_intrest
+             status|all)  corona.status                         ;;
+                  short)  corona.update ; corona.short "$2"     ;;
+                  table)  corona.table                          ;;
+           view|display)  corona.view "$2"                      ;;
+                   help)  corona.help                           ;;
+                    web)  firefox "$source_url"                 ;;
+                      *)  corona.status                         ;;
     esac
 }
 
@@ -51,17 +52,17 @@ corona.help() {
 
 
 corona.update() {
-    printf "upadating data... "
+    printf "updating data... "
     local _clone_location="/tmp/guru/corona"
     source_file="cases_country.csv"
 
     if ! [ -d "$_clone_location" ]; then
             mkdir -p "$_clone_location"
-            cd $_clone_location
+            cd "$_clone_location"
             git clone -b web-data https://github.com/CSSEGISandData/COVID-19.git
         fi
 
-    source_file=$_clone_location/$report_location/$source_file
+    source_file="$_clone_location/COVID-19/data/$source_file"
 
     cd "$_clone_location/COVID-19"
     if git pull >/dev/null 2>&1 ; then
@@ -82,7 +83,7 @@ corona.update() {
 
 corona.get_data () {
     local _location="$1"
-    _data="$(cat $source_file | grep """$_location""")"
+    _data="$(cat $source_file | grep $_location)"
     _data="${_data//'  '/'_'}"
     _data="${_data//' '/'_'}"
     _data="$(echo $_data | column -t -s ',')"
@@ -90,27 +91,27 @@ corona.get_data () {
 }
 
 
-corona.country_current_table () {
+corona.table () {
     corona.update
-    [ "$1" ] && location="$1"
-    corona.get_data "$location"
+    [ "$1" ] && country_selected="$1"
+    corona.get_data "$country_selected"
     printf "%s \t%s %s \t%s \n" "Confirmed" "Deaths" "Recovered" "Active" | column -t -s $" "
     printf "%s \t\t%s \t%s \t\t%s \n" "${data_list[4]}" "${data_list[5]}" "${data_list[6]}" "${data_list[7]}" | column -t -s $" "
 }
 
 
-corona.country_current_oneline () {
-    [ "$1" ] && location="$1"
+corona.short () {
+    [ "$1" ] && country_selected="$1"
     _last_time="$HOME/corona" ; [ -d "$_last_time" ] || mkdir "$_last_time"
-    _last_time="$_last_time/$location.last" ; [ -f "$_last_time" ] || touch "$_last_time"
+    _last_time="$_last_time/$country_selected.last" ; [ -f "$_last_time" ] || touch "$_last_time"
 
-    corona.get_data "$location"
+    corona.get_data "$country_selected"
 
     declare -a _last_list=($(cat $_last_time))
     declare -a _current_list=(${data_list[4]} ${data_list[5]} ${data_list[6]})
     local _change=""
 
-    printf "${NC}$location\t${CRY}%s\t${RED}%s\t${GRN}%s\t${NC}" "${data_list[4]}" "${data_list[5]}" "${data_list[6]}"
+    printf "${NC}$country_selected\t${CRY}%s\t${RED}%s\t${GRN}%s\t${NC}" "${data_list[4]}" "${data_list[5]}" "${data_list[6]}"
     if ! ((_current_list[0]==_last_list[0])) ; then
             _change=$((_current_list[0]-_last_list[0]))
 
@@ -137,19 +138,19 @@ corona.country_current_oneline () {
 }
 
 
-corona.country_current_intrest () {
+corona.status () {
     corona.update
     printf "${WHT}Country\tInfect\tDeath\tRecov\tChange ${NC}(since last check)\n"
     for _country in ${country_list[@]}; do
-            corona.country_current_oneline "$_country"
+            corona.short "$_country"
         done
 }
 
 
-corona.display () {
+corona.view () {
     local _sleep_time=3600 ; [ "$1" ] && _sleep_time=$1
     while : ; do
-            corona.country_current_intrest
+            corona.status
             sleep "$_sleep_time"
             corona.update
         done
