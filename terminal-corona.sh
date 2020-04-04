0e53da80fc785bfcc87907d5b9a128d67d53e8fb
casa <casa@ujo.guru>
2020-04-04 20:13:43 +0300

    history browsing to view passed

--- terminal-corona.sh
+++ terminal-corona.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # ujo.guru corona status viewer - a linux shell script to view current corona infection status per country. casa@ujo.guru 2020
 # data source is CSSE at Johns Hopkins University COVID-19 git database
-source_url="https://github.com/CSSEGISandData/COVID-19/blob/web-data/data/cases_country.csv"
 
-source list_of_countries.txt
+COUNTRY_LIST="Finland Sweden Russia Estonia Norway Iceland Denmark Germany France Italy Spain Greece Kingdom Iran India Thailand Vietnam China Japan US"
+COUNTRY_LIST_ALL="Australia Austria Canada China Denmark Finland France Germany Iceland Ireland Italy Netherlands Norway Russia Sweden Switzerland US Afghanistan Albania Algeria Andorra Angola Antigua Argentina Armenia Azerbaijan Bahamas Bahrain Bangladesh Barbados Belarus Belgium Belize Benin Bhutan Bolivia Bosnia Botswana Brazil Brunei Bulgaria Burkina Burma Burundi Cabo Cambodia Cameroon Central Chad Chile Colombia Congo Costa Cote Croatia Cuba Cyprus Czechia Diamond Djibouti Dominica Dominican Ecuador Egypt Equatorial Eritrea Estonia Eswatini Ethiopia Fiji Gabon Gambia Georgia Ghana Greece Grenada Guatemala Guinea Guinea-Bissau Guyana Haiti Honduras Hungary India Indonesia Iran Iraq Israel Jamaica Japan Jordan Kazakhstan Kenya Kosovo Kuwait Kyrgyzstan Laos Latvia Lebanon Liberia Libya Liechtenstein Lithuania Luxembourg Zaandam Madagascar Malaysia Maldives Mali Malta Mauritania Mauritius Mexico Moldova Monaco Mongolia Montenegro Morocco Mozambique Namibia Nepal Nicaragua Niger Nigeria Macedonia Oman Pakistan Panama Papua Paraguay Peru Philippines Poland Portugal Qatar Romania Rwanda Lucia Grenadines Marino Arabia Senegal Serbia Seychelles Sierra Singapore Slovakia Slovenia Somalia Spain Lanka Sudan Suriname Syria Taiwan Tanzania Thailand Timor-Leste Togo Trinidad Tunisia Turkey Uganda Ukraine Uruguay Uzbekistan US Kingdom Venezuela Vietnam Zambia Zimbabwe"
+
 declare -a country_list=($COUNTRY_LIST)
 country_selected="${country_list[0]}"
+source_url="https://github.com/CSSEGISandData/COVID-19/blob/web-data/data/cases_country.csv"
 
 # quick decorations from deco.sh for standalone
 export RED='\033[0;31m'
@@ -14,8 +16,11 @@ export WHT='\033[1;37m'
 export NC='\033[0m'
 export UPDATED="${GRN}UPDATED${NC}\n"
 export FAILED="${RED}FAILED${NC}\n"
-FAILED () {  [ "$1" ] && printf "$1: $FAILED" || printf "$FAILED"  ; }
-UPDATED () { [ "$1" ] && printf "$1: $UPDATED"           || printf "$UPDATED" ; }
+export DONE="${GRN}DONE${NC}\n"
+FAILED () { [ "$1" ] && printf "$1: $FAILED" || printf "$FAILED" ; }
+UPDATED () { [ "$1" ] && printf "$1: $UPDATED" || printf "$UPDATED" ; }
+DONE () { [ "$1" ] && printf "$1: $DONE" || printf "$DONE" ; }
+
 
 
 # User interface
@@ -25,7 +30,7 @@ corona.main () {
     local _cmd="$1" ; shift
 
     case $_cmd in
-         history|status|\
+          history|status|\
         view|raw|md|help)  corona.$_cmd "$@"                   ;;
                      all)  country_list=($COUNTRY_LIST_ALL)
                            corona.status "$@"                  ;;
@@ -36,40 +41,28 @@ corona.main () {
         esac
 }
 
-# tc  ҉19
-# C ҉VID-19
-# c ҉o҉r ҈o ҉n ҈a ҉
-#   ҉ COVID-19
-#Status shows on header
-    # spreading
-    # printf "  ҉" | hexdump # 0000000 2020 89d2 0000004
-    # weakening
-    # printf "  ҈" | hexdump # 0000000 2020 88d2 0000004
-    # still there
-    # printf "*"
-    # gone
-    # printf " "
-
-
-
 corona.help() {
     printf "${WHT}COVID-19 status viewer - help ----------------- casa@ujo.guru   ҉ ${NC}\n"
     printf "a Linux shell script to view current corona infection status worldwide\n"
     printf "\n${WHT}usage:${NC}\t terminal-corona -t|h [output] all|Country List \n"
     printf "\n${WHT}output:${NC}\n"
-    printf "  status [all|List Of Country]  colored table view \n"
-    printf "  txt                           tight text output \n"
-    printf "  csv                           csv output \n"
-    printf "  md                            markdown table \n"
-    printf "  raw 'separator'               raw output with selectable separator \n"
-    printf "  web                           open web view in source github page \n"
-    printf "  view -i 'sec'                 status loop, updates hourly or input\n"
-    printf "                                amount of seconds \n"
-    printf "  help                          help view \n\n"
+    printf "  status [all|List Of Country]   current table view \n"
+    printf "  history [Country]              table of history with changes \n"
+    printf "  txt                            tight text output \n"
+    printf "  csv                            csv output \n"
+    printf "  md                             markdown table \n"
+    printf "  raw 'separator'                raw output with selectable separator \n"
+    printf "  web                            open web view in source github page \n"
+    printf "  view -i 'sec'                  status loop, updates hourly or input (s) \n"
+    printf "                                 loop commands:\n"
+    printf "                                   n|p   jump to next or previous day \n"
+    printf "                                   h     headers on or off toggle \n"
+    printf "                                   t     time stamp toggle \n"
+    printf "                                   q     quit from loop \n"
+    printf "  help                           help view \n\n"
     printf "${WHT}flags:${NC}\n"
-    printf "  -t                            to activate timestamps \n"
-    printf "  -h                            set headers on or off \n\n"
-    printf "All except view can take argument 'all' to list all countries status \n"
+    printf "  -d                             date  \n\n"
+    printf "All except view history can take argument 'all' to list all countries status \n"
     printf "or list of country typed with capital first letter. If left blanc county \n"
     printf "of interest is used. Flags are place oriented and cannot be combined. \n"
     printf "\n${WHT}examples:${NC} "
@@ -85,7 +78,7 @@ corona.help() {
 ## Functional
 
 corona.update() {
-    printf "updating data.. "
+    #printf "updating git data.. "
     clone_location="/tmp/guru/corona"
     current_source_file="cases_country.csv"
     history_source_file="cases_time.csv"
@@ -96,14 +89,14 @@ corona.update() {
             git clone -b web-data https://github.com/CSSEGISandData/COVID-19.git
         fi
 
-
-    # cd "$clone_location/COVID-19"
-    # if git pull >/dev/null 2>&1 ; then
-    #         UPDATED
-    #     else
-             FAILED "database update"
-    #         return 10
-    #     fi
+    cd "$clone_location/COVID-19"
+    if git pull >/dev/null 2>&1 ; then
+            #UPDATED
+            echo >/dev/null
+        else
+            FAILED "git database update"
+            return 10
+        fi
 
     current_source_file="$clone_location/COVID-19/data/$current_source_file"
     history_source_file="$clone_location/COVID-19/data/$history_source_file"
@@ -117,12 +110,13 @@ corona.update() {
 }
 
 corona.get_history () {
+    #printf "analyzing history.. "
     local _stamp=""
     local _location="$1"
     local _output_file="$clone_location/$_location.history"
     local _temp_file="$clone_location/history.temp"
     local _data="$(cat $history_source_file | grep $_location)"
-    # _data="${_data//'  '/'_'}"
+
     _data="${_data//' '/'_'}"
     _data=${_data//,/ }
     echo "${_data[@]}" | cut -f 2-5 -d ' '> "$_temp_file"
@@ -130,29 +124,36 @@ corona.get_history () {
     if [[ -f $_output_file ]] ; then rm "$_output_file" ; fi
 
     while IFS= read -r line ; do
-      _stamp="$(echo $line | cut -f 1 -d ' ')"
-      _data="$(echo $line | cut -f 2-4 -d ' ')"
-      echo "$(date -d $_stamp '+%Y%m%d') $_data" >> "$_output_file"
-    done < "$_temp_file"
+            _stamp="$(echo $line | cut -f 1 -d ' ')"
+            _data="$(echo $line | cut -f 2-4 -d ' ')"
+            echo "$(date -d $_stamp '+%Y%m%d') $_data" >> "$_output_file"
+        done < "$_temp_file"
     sort "$_output_file" -o "$_output_file"
+    #UPDATED "$_location"
 }
 
 
 corona.get_data () {
     local _data=""
     local _location="$1"
-    #local current_date=$(date '+%Y%m%d')
-
-    #echo "${country_list[@]}:$_location"
-    #echo "$target_date:$current_date"
 
     if ! ((target_date==current_date)) ; then
-            corona.get_history "$_location"
+
+            if ! [[ -f "$clone_location/$_location.history" ]] ; then
+                    corona.get_history "$_location"
+                fi
+
+            local _date_modified=$(date -d $(stat "$clone_location/$_location.history" |grep Modify|cut -f2 -d ' ') +'%Y%m%d')
+
+            if ((_date_modified<current_date)) ; then
+                    corona.get_history "$_location"
+                fi
+
+
             local _history_file="$clone_location/$_location.history"
             _data="$_location 0 $(grep $target_date $_history_file) NA" # | cut -f 2-4 -d ' ')
-            current_data_list=($_data)
 
-            #echo "$_history_date $_history_file"
+            current_data_list=($_data)
         else
             _data="$(cat $current_source_file | grep $_location | head -1)"
             _data="${_data//'  '/'_'}"
@@ -161,7 +162,7 @@ corona.get_data () {
             local _data_list=($_data)
             _data_list[1]=$(date -d $(cut -f2 -d '_' <<< ${_data_list[1]}) '+%H:%M:%S')
             _data_list[2]=$(date -d $(cut -f1 -d '_' <<< ${_data_list[1]}) '+%Y%m%d')
-            #_data_list[2]=$(date -d ${_data_list[2]} '+%Y%m%d')
+
             current_data_list=(${_data_list[0]} ${_data_list[1]} ${_data_list[2]} \
                                ${_data_list[4]} ${_data_list[5]} ${_data_list[6]})
         fi
@@ -171,17 +172,10 @@ corona.get_data () {
 
 ## Print out
 
-corona.history () {
-    corona.update
-    corona.get_data "$1" "$2"
-    echo "${current_data_list[@]}"
-}
-
-
 corona.country () {
     [ "$1" ] && country_selected="$1"
 
-    _last_time="$HOME/corona" ; [ -d "$_last_time" ] || mkdir "$_last_time"
+    _last_time="$clone_location" ; [ -d "$_last_time" ] || mkdir "$_last_time"
     _last_time="$_last_time/$country_selected.last" ; [ -f "$_last_time" ] || touch "$_last_time"
 
     corona.get_data "$country_selected"
@@ -236,12 +230,11 @@ corona.status () {
     if [[ $header ]]; then
             #printf "${WHT}  ҉ terminal-corona %40s${NC}\n" " linux shell COVID-19 status viewer 2020"
             [[ $timestamp ]] && printf "${WHT}Updated     "
-            printf "${WHT}%15s,%7s,%7s,%7s,%7s ${NC}(since last check) \n" "Country" "Infect" "Death" "Recov" "Change" | column -t -s$','
+            printf "${WHT}%15s,%7s,%7s,%7s,%7s ${NC} \n" "Country" "Infect" "Death" "Recov" "Change" | column -t -s$','
         fi
     if [[ "$1" ]]; then country_list=("$@") ; fi
 
     for _country in ${country_list[@]} ; do
-
             corona.country "$_country" | column -t -s$','
         done
 
@@ -262,21 +255,52 @@ corona.view () {
     if [[ "$1" ]] ; then country_list=("$@") ; fi
 
     while : ; do
+            clear
             corona.status
-            read -t $_sleep_time -p "" _cmd
-
+            read -n1 -t $_sleep_time -p "" _cmd
             case $_cmd in
-                    q|exit|quit) break ;;
-                    t|timestamp) [[ $timestamp ]] && timestamp="" || timestamp=true ;;
-                       h|header) [[ $header ]]  && header="" || header=true ;;
-
+                    q)  break                                                           ;;
+                    t)  [[ $timestamp ]] && timestamp="" || timestamp=true              ;;
+                    h)  [[ $header ]]  && header="" || header=true                      ;;
+                    b)  target_date=$(date -d "$target_date - 1 days" +'%Y%m%d')
+                        ((target_date<20200220)) && target_date=20200220                ;;
+                    n)  target_date=$(date -d "$target_date + 1 days" +'%Y%m%d')
+                        ((target_date>current_date)) && target_date=${current_date}     ;;
                 esac
+
+        done
+}
+
+
+corona.history () {
+    corona.update
+
+    local _country="Finland" ; [[ "$1" ]] &&_country="$1" ; shift
+    local _from=$(date -d 20200220 +'%Y%m%d') ; [[ "$1" ]] &&_from=$(date -d $1 +'%Y%m%d') ; shift
+    local _to=$(date -d "$current_date -1 days" +'%Y%m%d') ; [[ "$1" ]] &&_to=$(date -d $1 +'%Y%m%d') ; shift
+    local _last_time="$clone_location/$_country.last"
+
+    [[ -f $_last_time ]] && rm $_last_time
+
+
+    if [[ $header ]]; then
+            [[ $timestamp ]] && printf "${WHT}Updated     "
+            printf "${WHT}%15s,%7s,%7s,%7s,%7s ${NC} \n" "Country" "Infect" "Death" "Recov" "Change" | column -t -s$','
+        fi
+
+
+    while [[ $_from -le $_to ]] ; do
+            _from=$(date -d "$_from + 1 day" +"%Y%m%d")
+            target_date=$_from
+            corona.country "$_country" | column -t -s$','
         done
 }
 
 
 corona.md () {
+
     corona.update
+
     if [[ "$1" ]] ; then country_list=("$@") ; fi
     if [[ "${1,,}" == "all" ]] ; then country_list=("$COUNTRY_LIST_ALL") ; fi
 
@@ -296,10 +320,12 @@ corona.md () {
 
 
 corona.raw () {
+
     corona.update
 
     local _output=""
     local _separator=" "
+
     if [[ "$1" ]] ; then _separator="$1" ; shift ; fi
     if [[ "$1" ]] ; then country_list=("$@") ; fi
     if [[ "${1,,}" == "all" ]] ; then country_list=("$COUNTRY_LIST_ALL") ; fi
@@ -311,7 +337,6 @@ corona.raw () {
             printf "%s$_separator%s$_separator%s$_separator%s$_separator%s\n" \
             "${current_data_list[0]}" "${current_data_list[3]}" "${current_data_list[4]}" "${current_data_list[5]}" "${current_data_list[2]}_${current_data_list[1]}"
         done
-    #echo "1:'$1' | h:'$header' s:'$_separator' 'o:$_output' 'cl:$country_list'"
     return 0
 }
 
@@ -324,7 +349,10 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 
     while getopts 'di' flag; do
             case "${flag}" in
-                d)  target_date=$(date -d ${2} '+%Y%m%d') ; shift ; shift ;;
+                d)  target_date=$(date -d ${2} '+%Y%m%d')
+                    if ((target_date<20200220)) || ((target_date>current_date)); then
+                            target_date=${current_date}
+                        fi ; shift ; shift ;;
             esac
         done
 
@@ -341,3 +369,18 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     exit 0
 fi
 
+
+
+# tc  ҉19
+# C ҉VID-19
+# c ҉o҉r ҈o ҉n ҈a ҉
+#   ҉ COVID-19
+#Status shows on header
+    # spreading
+    # printf "  ҉" | hexdump # 0000000 2020 89d2 0000004
+    # weakening
+    # printf "  ҈" | hexdump # 0000000 2020 88d2 0000004
+    # still there
+    # printf "_"
+    # gone
+    # printf " "
